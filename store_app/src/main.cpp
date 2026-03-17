d6037772598e5e0dcaa529d39b98ea45770cf44c
Felipe Pesantez <felipecgdesign@gmail.com>
2026-03-17 09:34:28 +0000

    added store_app structure

---
+++ store_app/src/main.cpp
@@ -0,0 +1,9 @@
+#include "test.hpp"
+
+int main(){
+
+    Test test = Test();
+    test.Greeting();
+
+    return 0;
+}
---
+++ store_app/src/test.cpp
@@ -0,0 +1,5 @@
+#include "test.hpp"
+
+void Test::Greeting(){
+    std::cout << "Hellooooo\n";
+}
